f351e01b6e51be339bd3460a2c71d368f00b9e44
oyebz <oyetayosamuel0@gmail.com>
2022-12-11 15:50:00 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single numbers of base10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
