a4e24128667cd9133fa11c2922e4ea44068abb0f
oyebz <oyetayosamuel0@gmail.com>
2022-12-12 05:38:01 -0800

    do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	int n, m, o;
+	int n, m, l;
 
 	for (n = 48; n <= 58; n++)
 	{
 		for (m = 49; m <= 58; m++)
 		{
-			for (o = 50; o <= 58; o++)
+			for (l = 50; l <= 58; l++)
 			{
-				if (o > m && m > n)
+				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
-					putchar(o);
+					putchar(l);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
