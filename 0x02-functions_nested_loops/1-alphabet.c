ac45361d607630bb68a32f2fc87b5c5f68ae35df
oyebz <oyetayosamuel0@gmail.com>
2022-12-13 19:40:35 -0800

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
