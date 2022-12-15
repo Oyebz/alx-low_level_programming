0de23e3de8296947f9b4ac9dae5bc0aa222e3083
oyebz <oyetayosamuel0@gmail.com>
2022-12-15 03:06:42 -0800

    errs

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,16 +7,10 @@
 
 void print_alphabet(void)
 {
-	char c;
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-
-	_putchar('\n');
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+		_putchar('\n');
 
 }
 
