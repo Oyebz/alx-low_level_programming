7a110934f1a416503ce70bc206a3b98fe800d39a
oyebz <oyetayosamuel0@gmail.com>
2022-12-15 17:46:09 -0800

    header file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks for uppercase character
+ *
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	int c;
+
+	c = ("A");
+	if (c < "Z") 
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
