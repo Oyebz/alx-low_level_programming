8dc9faa10c5f6d1e421c8f82f9b8771cff8cee36
oyebz <oyetayosamuel0@gmail.com>
2022-12-19 19:43:52 -0800

    stylistic consequences

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: input string
+ * Return: string in reverse
+ */
+
+void rev_string(char *s)
+{
+char rev = s[0];
+int smooce = 0;
+int i;
+while (s[smooce] != '\0')
+smooce++;
+
+for (i = 0; i < smooce; i++
+{
+smooce--;
+rev = s[i];
+s[i] = s[smooce];
+s[smooce] = rev;
+}
+}
