8c33046fa04239268133fd2220bf4a404803c1fd
oyebz <oyetayosamuel0@gmail.com>
2022-12-22 17:26:32 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,13 @@
 
 char *_strncat(char *dest, char *src, int n);
 {
-	int c, i;
+	int d, i;
 
-	c = 0;
+	d = 0;
 
-	while (dest[c])
+	while (dest[d])
 	{
-		c++;
+		d++;
 	}
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
