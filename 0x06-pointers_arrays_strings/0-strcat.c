767c3d09895422c8d6c12df2318937f8ddb78eb0
oyebz <oyetayosamuel0@gmail.com>
2022-12-23 17:53:36 -0800

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,23 +8,15 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
+int c, c2;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
+c = 0;
 
-j = 0;
-while (src(dest[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
+while (dest[c])
+c++;
 
-dest[i] = '\0';
+for (c2 = 0; src[c2]; c2++)
+dest[c++] = src[c2];
+
 return (dest);
 }
