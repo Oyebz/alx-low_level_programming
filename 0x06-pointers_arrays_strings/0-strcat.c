34689c5f6eded4142432008cebd5c3a420d8790b
oyebz <oyetayosamuel0@gmail.com>
2022-12-23 18:40:24 -0800

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,16 @@ int c, c2;
 
 c = 0;
 
-while (dest[c])
-c++;
-
-for (c2 = 0; src[c2] != 0; c2++)
+while (dest[c] != '\0')
 {
-dest[c++] = src[c2];
-c += 1;
+c++;
+}
+c2 = 0;
+while (src[c2] != '\0')
+{
+dest[c] = src[c2];
+c++;
+c2++;
 }
 dest[c] = '\0';
 return (dest);
